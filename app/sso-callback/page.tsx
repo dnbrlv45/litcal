3d82b06f7eecdcec002d2619cc5ddee5a7600e5a
dnbrlv45 <dnbrlv45@gmail.com>
2026-05-31 22:50:06 -0700

    Fix Clerk SSO callback export

--- app/sso-callback/page.tsx
+++ app/sso-callback/page.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import { HandleSSOCallback } from "@clerk/nextjs";
+import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function SSOCallbackPage() {
   return (
-    <HandleSSOCallback
-      navigateToApp={({ session, decorateUrl }) => {
-        const destination = session?.currentTask ? `/sign-in/tasks/${session.currentTask.key}` : "/";
-        const url = decorateUrl(destination);
-        window.location.href = url;
-      }}
-      navigateToSignIn={() => {
-        window.location.href = "/sign-in";
-      }}
-      navigateToSignUp={() => {
-        window.location.href = "/sign-up";
-      }}
+    <AuthenticateWithRedirectCallback
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+      signInForceRedirectUrl="/"
+      signUpForceRedirectUrl="/"
+      continueSignUpUrl="/sign-up"
     />
   );
 }
